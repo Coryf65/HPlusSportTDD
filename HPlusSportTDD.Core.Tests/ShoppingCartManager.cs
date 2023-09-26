b9d0aecf384b1d444a093017cdb4e6ab02e8a113
Cory Fabian <Coryf65@users.noreply.github.com>
2023-09-25 21:12:33 -0500

    completing a second test

--- HPlusSportTDD.Core.Tests/ShoppingCartManager.cs
+++ HPlusSportTDD.Core.Tests/ShoppingCartManager.cs
@@ -2,11 +2,20 @@ namespace HPlusSportTDD.Core.Tests;
 
 public record ShoppingCartManager
 {
+    private List<AddToCartItem> _shoppingCart;
+
+    public ShoppingCartManager()
+    {
+        _shoppingCart = new List<AddToCartItem>();
+    }
+    
     public AddToCartResponse AddToCart(AddToCartRequest request)
     {
+        _shoppingCart.Add(request.Item);
+        
         return new AddToCartResponse()
         {
-            Items = new[] { request.Item }
+            Items = _shoppingCart.ToArray()
         };
     }
 }
--- HPlusSportTDD.Core.Tests/ShoppingCartTests.cs
+++ HPlusSportTDD.Core.Tests/ShoppingCartTests.cs
@@ -32,4 +32,42 @@ public class ShoppingCartTests
         Assert.NotNull(response);
         Assert.Contains(item, response.Items);
     }
+    
+    [Test]
+    public void ShouldReturnArticlesAddedToCart()
+    {
+        // Arrange
+        var manager = new ShoppingCartManager();
+        
+        var item1 = new AddToCartItem()
+        {
+            ArticleId = 42,
+            Quantity = 5
+        };
+        var item2 = new AddToCartItem()
+        {
+            ArticleId = 78,
+            Quantity = 2
+        };
+
+        var request = new AddToCartRequest()
+        {
+            Item = item1
+        };
+        
+        // Assert
+        AddToCartResponse response = manager.AddToCart(request);
+
+        request = new AddToCartRequest()
+        {
+            Item = item2
+        };
+        
+        response = manager.AddToCart(request);
+        
+        // Assert
+        Assert.NotNull(response);
+        Assert.Contains(item1, response.Items);
+        Assert.Contains(item2, response.Items);
+    }
 }
